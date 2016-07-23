c295feb89ba15c752d7bed0390601fc8b48efb87
janeve.g <janeve.g@endurance.com>
2016-07-23 14:53:12 +0530

    Adding step1 code.

--- src/main/java/com/endurance/india/Main.java
+++ src/main/java/com/endurance/india/Main.java
@@ -1,7 +1,17 @@
 package com.endurance.india;
 
-/**
- * Created by janeve.g on 23/07/16.
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
+        MessagePrinter bean = ctx.getBean(MessagePrinter.class);
+
+        bean.printMessage();
+
+        ((ClassPathXmlApplicationContext) (ctx)).close();
+    }
+
 }
--- src/main/java/com/endurance/india/MessagePrinter.java
+++ src/main/java/com/endurance/india/MessagePrinter.java
@@ -1,8 +1,29 @@
 package com.endurance.india;
 
 public class MessagePrinter {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    private String message;
+    private int iterations;
+
+    public void printMessage(){
+        for (int i = 0; i < iterations; i++) {
+            System.out.println(i+1 + ": " + message);
+        }
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getIterations() {
+        return iterations;
+    }
+
+    public void setIterations(int iterations) {
+        this.iterations = iterations;
     }
 }
