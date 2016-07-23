cba866dc5066a0d9b40823a35f4ee8efc48b738a
janeve.g <janeve.g@endurance.com>
2016-07-23 15:24:58 +0530

    Adding new lines for better visibility of the demo.

--- src/main/java/com/endurance/india/Main.java
+++ src/main/java/com/endurance/india/Main.java
@@ -9,7 +9,9 @@ public class Main {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
         MessagePrinter bean = ctx.getBean(MessagePrinter.class);
 
+        System.out.print("\n\n");
         bean.printMessage();
+        System.out.print("\n\n");
 
         ((ClassPathXmlApplicationContext) (ctx)).close();
     }
