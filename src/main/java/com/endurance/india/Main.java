cc53e472867c29687cbf9b34b597506604e958ba
janeve.g <janeve.g@endurance.com>
2016-08-04 16:36:39 +0530

    Code for showing Bean Scopes added.

--- src/main/java/com/endurance/india/Main.java
+++ src/main/java/com/endurance/india/Main.java
@@ -5,13 +5,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
-        MessagePrinter bean = ctx.getBean(MessagePrinter.class);
+
+        System.out.println("Is the Bean 'singleton'? " + ctx.isSingleton("printer"));
+        System.out.println("Is the Bean 'prototype'? " + ctx.isPrototype("printer"));
 
         System.out.print("\n\n");
+        MessagePrinter bean = (MessagePrinter) ctx.getBean("printer");
+        bean.printMessage();
+        bean.setMessage(">>> New Message Set in the Bean.");
         bean.printMessage();
         System.out.print("\n\n");
+        MessagePrinter anotherBean = (MessagePrinter) ctx.getBean("printer");
+        anotherBean.printMessage();
+        System.out.print("\n\n");
+
+        Thread.sleep(1500);
 
         ((ClassPathXmlApplicationContext) (ctx)).close();
     }
